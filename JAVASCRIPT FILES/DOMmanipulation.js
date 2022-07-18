598fde5a875e64dc601658c7de1e06ca30280518
nitish <nitishrsm@gmail.com>
2022-07-18 07:30:55 +0530

    style.css,practicing-dom-manipulation,dommanipulation.js and adding new files in a folder named projects(project1^C

--- JAVASCRIPT FILES/DOMmanipulation.js
+++ JAVASCRIPT FILES/DOMmanipulation.js
@@ -1,3 +1,28 @@
-const ul = document.querySelector('.class-1')
-// document.write(ul)
-console.log(ul)
+const ul = document.getElementsByTagName('li')
+ul[0].style.color = 'white';
+ul[1].style.color = 'white';
+ul[2].style.color = 'white';
+ul[3].style.color = 'white';
+ul[4].style.color = 'white';
+ul[5].style.color = 'white';
+const h1 = document.getElementsByTagName('h1')
+// console.log(h1)
+h1[0].style.color = 'blue';
+h1[0].style.padding = "20px";
+const parent = document.getElementsByClassName('parent')
+console.log(parent[0])
+parent[0].style.border = "5px solid blue"
+parent[0].style.borderRadius = "20px"
+parent[0].style.fontFamily = 'cursive'
+ul[0].style.margin = "20px"
+ul[1].style.margin = "20px"
+ul[2].style.margin = "20px"
+ul[3].style.margin = "20px"
+ul[4].style.margin = "20px"
+ul[5].style.margin = "20px"
+ul[0].style.borderRadius = "20px"
+ul[1].style.borderRadius = "20px"
+ul[2].style.borderRadius = "20px"
+ul[3].style.borderRadius = "20px"
+ul[4].style.borderRadius = "20px"
+ul[5].style.borderRadius = "20px"
